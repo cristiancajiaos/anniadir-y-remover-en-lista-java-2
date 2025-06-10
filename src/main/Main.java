1dd0349eccaf8c158fce07b3dd1be8c0ef43a9a2
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-10 09:26:10 -0400

    Primer commit

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import view.Vista;
+
+public class Main {
+    public static void main(String[] args) {
+        Vista vista = new Vista();
+        vista.setVisible(true);
+    }
+
+}
